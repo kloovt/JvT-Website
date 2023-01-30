beb15bc3b8eea87016ebfa0bbd95cc21dbfb8424
kloovt <kloovtvantriest@gmail.com>
2023-01-30 12:35:50 +0100

    adding the first version

---
+++ pages/_document.js
@@ -0,0 +1,50 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  const meta = {
+    title: 'JorritvTriest home page',
+    description: 'The home page for my server',
+    image: 'logo.png'
+  }
+
+  return (
+    <Html lang="en">
+      <Head>
+        <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+        <link href="/static/favicons/site.webmanifest" rel="manifest" />
+        <link
+          href="/static/favicons/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/static/favicons/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/static/favicons/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          color="#4a9885"
+          href="/static/favicons/safari-pinned-tab.svg"
+          rel="mask-icon"
+        />
+        <meta name="robots" content="noindex" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
